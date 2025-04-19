e3baea08e45540916d7374c1a87245f2a5972045
ISHA SHAH <ishashah0503@gmail.com>
2025-04-18 21:08:56 -0400

    fullstackassignment

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ const path = require("path");
 const sessions = require("express-session");
 const dotenv = require("dotenv");
 
+const cors = require("cors");
 
+
+//used to load the environment variable stored in .env loacl file
 dotenv.config();
 
+// initializing the express environment using variable apps 
+// (using it as json object to use express methods) and defining port to default or 8888
 const app = express();
-
-
-const port = process.env.PORT || "8888";
-
-
+const port = process.env.PORT || '8888';
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://react-assignment-ishas-projects-1e8fb4df.vercel.app/",
+    "hthttps://fullstackassignment1-iga3.onrender.com"
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  }));
 app.set("views",path.join(__dirname,"views"));
 
 
